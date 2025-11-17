e12f05d93231241a217ada9b3ddb69e100e52147
MonikaRS <your-email@example.com>
2025-11-18 01:09:57 +0700

    Improve Parameterized test names and add assertion in OrderTest

--- src/test/java/ru/yandex/scooter/tests/OrderTest.java
+++ src/test/java/ru/yandex/scooter/tests/OrderTest.java
@@ -9,9 +9,11 @@ import ru.yandex.scooter.pageobjects.OrderPage;
 import java.util.Arrays;
 import java.util.Collection;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(Parameterized.class)
 public class OrderTest extends BaseTest {
-    
+
     private final String orderButtonType;
     private final String name;
     private final String surname;
@@ -21,9 +23,9 @@ public class OrderTest extends BaseTest {
     private final String rentalPeriod;
     private final String color;
     private final String comment;
-    
-    public OrderTest(String orderButtonType, String name, String surname, String address, 
-                    String phone, String date, String rentalPeriod, String color, String comment) {
+
+    public OrderTest(String orderButtonType, String name, String surname, String address,
+                     String phone, String date, String rentalPeriod, String color, String comment) {
         this.orderButtonType = orderButtonType;
         this.name = name;
         this.surname = surname;
@@ -34,43 +36,42 @@ public class OrderTest extends BaseTest {
         this.color = color;
         this.comment = comment;
     }
-    
-    @Parameterized.Parameters
+
+    @Parameterized.Parameters(name = "Заказ через {0} кнопку: {1} {2}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-            // Первый набор данных - верхняя кнопка заказа
-            {"top", "Иван", "Иванов", "ул. Ленина, д. 1", "+79991234567", "25.12.2024", "сутки", "black", "Позвонить за час"},
-            // Второй набор данных - нижняя кнопка заказа  
-            {"bottom", "Мария", "Петрова", "пр. Мира, д. 15", "+79997654321", "26.12.2024", "двое суток", "grey", "Оставить у двери"}
+                // Первый набор данных - верхняя кнопка заказа
+                {"top", "Иван", "Иванов", "ул. Ленина, д. 1", "+79991234567", "25.12.2024", "сутки", "black", "Позвонить за час"},
+                // Второй набор данных - нижняя кнопка заказа
+                {"bottom", "Мария", "Петрова", "пр. Мира, д. 15", "+79997654321", "26.12.2024", "двое суток", "grey", "Оставить у двери"}
         });
     }
-    
+
     @Test
     public void testSuccessfulOrder() {
         HomePage homePage = new HomePage(driver);
         OrderPage orderPage = new OrderPage(driver);
-        
+
         // Нажимаем кнопку заказа в зависимости от параметра
         if ("top".equals(orderButtonType)) {
             homePage.clickOrderButtonTop();
         } else {
             homePage.clickOrderButtonBottom();
         }
-        
+
         // Заполняем первую часть формы
         orderPage.fillFirstStep(name, surname, address, phone);
-        
+
         // Заполняем вторую часть формы
         orderPage.fillSecondStep(date, rentalPeriod, color, comment);
-        
+
         // Подтверждаем заказ
         orderPage.confirmOrder();
-        
+
         // Проверяем успешное создание заказа
         boolean isSuccess = orderPage.isOrderSuccess();
-        
-        if (!isSuccess) {
-            System.out.println("ВНИМАНИЕ: Возможен баг в приложении. Заказ не был успешно создан.");
-        }
+
+        // Добавляем assert для проверки
+        assertTrue("Заказ не был успешно создан для клиента: " + name + " " + surname, isSuccess);
     }
 }
